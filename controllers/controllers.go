ab9070802f74c6f0c93913b963f008ce809a8b83
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-07-20 10:00:34 +0530

    [Day3]-Added required func in respective files

---
+++ controllers/controllers.go
@@ -0,0 +1,19 @@
+package controllers
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func Signup() {}
+
+func Login() {}
+
+func ProductviewerAdmin() {}
+
+func SearchProduct() {}
+
+func SearchProductByQuery() {}
