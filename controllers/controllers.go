2a6157bc7c2cc0ee7349b60066b3dcecde63c023
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-07-22 10:16:51 +0530

    [Day4]-Created db client

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,5 +1,16 @@
 package controllers
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vikas-gautam/ecommerce-cart/models"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 func HashPassword(password string) string {
 
 }
@@ -8,7 +19,31 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 
 }
 
-func Signup() {}
+func Signup() {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := Validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+
+		}
+	}
+}
 
 func Login() {}
 
--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,29 @@
 package database
+
+import "errors"
+
+var (
+	ErrCantFindProduct    = errors.New("can't find the product")
+	ErrCantDecodeProduct  = errors.New("can't find the product")
+	ErrUserIdIsNotValid   = errors.New("this user is not valid")
+	ErrCantUpdateUser     = errors.New("can't add this product to the cart")
+	ErrCantRemoveItemCart = errors.New("can't remove this item from the cart")
+	ErrCantGetItem        = errors.New("was unable to get the item from the cart")
+	ErrCantBuyCartItem    = errors.New("can't update the purchase")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveCartItem() {
+
+}
+
+func BuyItemFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
