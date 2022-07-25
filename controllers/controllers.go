b63c4bb14d9dddb4dbf047a19cf3097ee4eeff20
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-07-25 10:20:21 +0530

    [Day5]-completed signup and login func

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/vikas-gautam/ecommerce-cart/models"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func HashPassword(password string) string {
@@ -40,12 +41,80 @@ func Signup() {
 		if err != nil {
 			log.Panic(err)
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
-
+			return
 		}
+
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": user already exists})
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+			return
+		}
+		if count > 0 {
+			c.JSON(http.StatusBadRequest, gin.H{"error": This phone number is  already in use})
+			return
+		}
+		password := HashPassword(*user.Password)
+		user.Password = &password
+
+		user.Created_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.Updated_At, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		user.ID = primitive.NewObjectID()
+		user.User_ID = user.ID.Hex()
+		token, refreshtoken, _ := generate.TokenGenerator(*user.Email, *user.First_Name, *user.Last_Name, user.User_ID)
+		user.Token = &token
+		user.Refresh_Token = &refreshtoken
+		user.UserCart = make([]models.ProductUser, 0)
+		user.Address_Details = make([]models.Address, 0)
+		user.Order_Status = make([]models.Order, 0)
+		_, Inserterr := UserCollection.InsertOne(ctx, user)
+		if Inserterr != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"user did not get created"})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusCreated, "successfully signed in!")
 	}
 }
 
-func Login() {}
+func Login() {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+		if err := c.BindJSON(&user); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+			return
+		}
+
+		err := UserCollection.FindOne(ctx, bson.M{"email": user.Email}).Decode(&founduser)
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "login or password incorrect"})
+			return
+		}
+		PasswordIsValid. msg := VerifyPassword(*user.Password, *founduser.Password)
+
+		defer cancel()
+        
+		if !PasswordIsValid{
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			fmt.Println(msg)
+			return
+		}
+		token, refreshtoken, _ := generate.TokenGenerator(*founduser.Email, *founduser.First_Name, *founduser.Last_Name, *founduser.User_ID)
+		defer cancel()
+
+		generate.UpdateAllTokens(token, refreshtoken, founduser.User_ID)
+		c.JSON(http.StatusFound, founduser)
+}
 
 func ProductviewerAdmin() {}
 
