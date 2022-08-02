6c3d402e4be5bafa7803e26bd7c67d5924c317a0
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-08-02 10:24:36 +0530

    [Day9]-Added searchproductByQuery func and deleteAddress func

--- controllers/address.go
+++ controllers/address.go
@@ -1,6 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/vikas-gautam/ecommerce-cart/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 func AddAddress() gin.HandlerFunc {
 
@@ -10,4 +19,33 @@ func EditHomeAddress() gin.HandlerFunc {}
 
 func EditWorkAddress() gin.HandlerFunc {}
 
-func DeleteAddress() gin.HandlerFunc {}
+func DeleteAddress() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		user_id := c.Query("id")
+
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"Error": "Invalid Search Index"})
+			c.Abort()
+			return
+		}
+
+		addresses := make([]models.Address, 0)
+		usert_id, err := primitive.ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal server error")
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		filter := bson.D(primitive.E{key: "_id", Value: usert_id})
+		update := bson.D{{key: "$set", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}}
+		_, err = UserCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.IndentedJSON(404, "Wrong command")
+			return
+		}
+		defer cancel()
+		ctx.Done()
+		c.IndentedJSON(200, "Successfully deleted")
+	}
+}
--- controllers/controllers.go
+++ controllers/controllers.go
@@ -176,5 +176,42 @@ func SearchProduct() gin.HandlerFunc{
 }
 
 func SearchProductByQuery() gin.HandlerFunc{
-	
+	return func(c *gin.Context){
+		var searchProducts []models.Product
+		queryParam := c.Query("name")
+		//u want to check if it's empty
+		
+		if queryParam == ""{
+			log.Println("query is empty")
+			c.Header("Content-Type", "application/json")
+			c.JSON(HTTP.StatusNotFound, gin.H{"Error":"Invalid search index"})
+			c.Abort()
+			return
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		
+		searchquerydb, err := ProductCollection.Find(ctx, bson.M{"product_name": bson.M{"$regex":queryParam}})
+		if err != nil{
+			c.IndentedJSON(404, "something went wrong while fetching the data")
+			return
+		}
+
+		searchquerydb.All(ctx, searchProducts)
+		if err != nil{
+			log.Println(err)
+			c.IndentedJSON(400, "Invalid")
+			return
+		}
+
+		defer searchquerydb.Close(ctx)
+		if err := searchquerydb.Err(); err != nil{
+			log.Println(err)
+			c.IndentedJSON(400, "Invalid request")
+			return
+		}
+		defer cancel()
+		c.IndentedJSON(200, searchProducts)
+	}
 }
