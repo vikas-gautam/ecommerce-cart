e1736c463cec49af9569fdcdba77b59c82c2e74a
Vikash Gautam <vikash.gautam.cn@freecharge.in>
2022-08-05 10:52:51 +0530

    [Day10]-func getItemFromCart

--- controllers/cart.go
+++ controllers/cart.go
@@ -3,9 +3,12 @@ package controllers
 import (
 	"context"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/vikas-gautam/ecommerce-cart/database"
+	"github.com/vikas-gautam/ecommerce-cart/models"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -149,4 +152,47 @@ func (app *Application) InstantBuy() gin.HandlerFunc {
 	c.IndentedJSON(200, "your order placed successfully!")
   }	
 
-func GetItemFromCart() gin.HandlerFunc {}
+func GetItemFromCart() gin.HandlerFunc {
+	return func(c *gin.Context){
+		user_id := c.Query("id")
+		if user_id == ""{
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H("error":"Invalid id")
+			c.Abort()
+			return
+		}
+
+		usert_id, _ := primitive.ObjectIDFromHex(user_id)
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		var filledcart models.User
+		UserCollection.FindOne(ctx, bson.D{primitive.E{Key:"_id", Value: usert_id}}).Decode(&filledcart)
+        if err != nil{
+			log.println(err)
+			c.IndentedJSON(500, "not found")
+			return
+		}
+
+		filter_match := bson.D{{Key:"$match", Value: bson.D{primitive.E{Key: "_id", Value: usert_id}}}} 
+		
+		//after finding that user, the found user must be having cart items (now bind will help u to work with that data)
+		unwind := bson.D{{Key: "$unwind", Value: bson.D{primitive.E{Key: "path", Value:"$usercart"}}}}
+		
+		//Addition of all the value of items in cart
+		grouping := bson.D{{Key: "$group", Value: bson.D{primitive.E{Key: "_id", Value:"$_id"}, {Key:"total", Value:bson.D{primitive.E{Key: "$sum", Value:"$usercart.price"}} }}}}
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{filter_match, unwind, grouping})
+		if err != nil{
+			log.Println(err)
+		}
+		var listing []bson.M
+		if err = pointcursor.All(ctx, &listing); err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+		}
+		for _, json := range listing{
+			c.IndentedJSON(200, json["total"])
+			c.IndentedJSON(200, filledcart.UserCart)
+		}
+		ctx.Done()
+	}
+}
